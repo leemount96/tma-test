c5302b4fbed8a42be8594a840d7322d8944a2fd9
Lee <lee@euler.xyz>
2024-09-20 15:06:51 +0800

    updating backedn logic

--- bot.js
+++ bot.js
@@ -6,18 +6,22 @@ bot.command('start', (ctx) => {
   ctx.reply('Welcome to ₿earn! Click the button below to open the mini app:', {
     reply_markup: {
       inline_keyboard: [[
-        { text: "Open Mini App", web_app: { url: "https://leemount96.github.io/tma-test" } }
+        { text: "Open ₿earn App", web_app: { url: "https://leemount96.github.io/tma-test" } }
       ]]
     }
   });
 });
 
+// This handler is necessary for Vercel serverless function
 module.exports = async (req, res) => {
   try {
     await bot.handleUpdate(req.body);
-    res.status(200).send('OK');
+    res.status(200).end();
   } catch (error) {
     console.error('Error in bot update handler:', error);
-    res.status(500).send('Internal Server Error');
+    res.status(500).end();
   }
 };
+
+// If you want to test the bot locally, uncomment this:
+// bot.launch();
